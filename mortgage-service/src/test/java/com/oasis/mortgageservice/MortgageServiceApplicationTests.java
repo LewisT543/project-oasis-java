28c701248f0744a06010bc9465d12b4797e45a5c
Lewis <lewisthornton@btinternet.com>
2024-03-29 14:11:47 +0000

    mortgage-service merged in

---
+++ mortgage-service/src/test/java/com/oasis/mortgageservice/MortgageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis.mortgageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
