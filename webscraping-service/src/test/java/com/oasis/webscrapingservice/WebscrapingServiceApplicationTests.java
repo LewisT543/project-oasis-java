ae04765b5d39a128fb438cff93cb16d6e40e4be6
Lewis <lewisthornton@btinternet.com>
2024-04-24 23:26:32 +0100

    Added dummy data

---
+++ webscraping-service/src/test/java/com/oasis/webscrapingservice/WebscrapingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis.webscrapingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscrapingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
